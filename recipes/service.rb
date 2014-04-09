43d3c4a61a186345c9f98211633e82668c09b6a1
Robert Coleman <github@robert.net.nz>
2014-04-10 07:24:47 +1200

    support for Upstart based services

--- recipes/service.rb
+++ recipes/service.rb
@@ -6,10 +6,20 @@ when "init"
     group "root"
     mode "0755"
   end
+  service "remote_syslog" do
+    action [:start, :enable]
+  end
+when "upstart"
+  cookbook_file "/etc/init/remote_syslog.conf" do
+    source "remote_syslog.upstart.conf"
+    owner "root"
+    group "root"
+    mode "0755"
+  end
+  service "remote_syslog" do
+    provider Chef::Provider::Service::Upstart
+    action [:start, :enable]
+  end
 else
   raise "Unsupported init style"
-end
-
-service "remote_syslog" do
-  action [:start, :enable]
 end
